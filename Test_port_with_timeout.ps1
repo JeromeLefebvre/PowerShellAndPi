1e610f6da7620a24454cc4a59955b604ffbb75d6
Jerome Lefebvre <jerome.p.lefebvre@gmail.com>
2015-05-19 09:15:31 +0900

    A simple Powershell with timeout

---
+++ Test_port_with_timeout.ps1
@@ -0,0 +1,22 @@
+﻿
+$socket = new-Object system.Net.Sockets.TcpClient 
+        
+$attempt = $socket.BeginConnect("PIServer",5450,$null,$null) 
+
+# ミリ秒単位でのタイムアウト
+# タイムアウトが送るとWaitOneの変数を降らしてみる
+$result = $attempt.AsyncWaitHandle.WaitOne(120000,$false) 
+
+If (-Not $result) {
+    Write-Host 'タイムアウトが起こりました'
+} Else {
+    $error.clear()
+    $socket.EndConnect($attempt) | out-Null 
+    If ($Error[0]) {
+        Write-Host "ポートが開いていない可能性があります"
+        Write-warning ("{0}" -f $error[0].Exception.Message)
+    } Else {
+        Write-Host "ポートが開いています"
+    }
+}
+
